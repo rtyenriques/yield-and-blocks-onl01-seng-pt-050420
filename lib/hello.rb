b003712be572cc6b30d649b9aa0cf95cfab89d72
harmonious-cable-1188 <rtyenriques@gmail.com>
2020-05-13 02:41:13 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,20 @@
-def hello_t
+def hello_t(array)
+if block_given?
+  i = 0
 
+  while i < array.length
+    yield array[i]
+    i = i + 1
+  end
+  array
+else
+  puts "Hey! No block was given!"
 end
-
+end
 # call your method here!
-
+brothers_with_T = ["Tim", "Tom", "Jim"]
+hello_t(brothers_with_T) do |name| #parameter for array[i] from yield
+   if name.start_with?("T")
+      puts "Hi, #{name}"
+   end
+end
