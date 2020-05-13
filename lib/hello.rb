ca93f72483c2386fa71608f864035f86b0584cc4
harmonious-cable-1188 <rtyenriques@gmail.com>
2020-05-13 02:13:44 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -12,7 +12,7 @@ end
 
 # call your method here!
 
-hello_t(["Tim", "Tom", "Jim"].each) do |name|
+hello_t(["Tim", "Tom", "Jim"]).each do |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
   end
